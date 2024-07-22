87fb2cf69216d7ce8785145f6171fd32c6b4982d
deivideguilherme <deivide.jau@gmail.com>
2024-07-22 17:27:17 -0300

    Configurado o eslint + prettier 100%, e criado os estilos globais

---
+++ devbills-front/src/app.tsx
@@ -0,0 +1,10 @@
+import GlobalStyles from './styles/globalstyles';
+
+export function App() {
+  return (
+    <>
+      <GlobalStyles />
+      <h1> DevBills </h1>
+    </>
+  );
+}
---
+++ devbills-front/src/styles/globalstyles.ts
@@ -0,0 +1,34 @@
+import { createGlobalStyle } from 'styled-components';
+
+import { theme } from './theme';
+
+export default createGlobalStyle`
+  *{
+    margin: 0;
+    padding: 0;
+    outline: none;
+    box-sizing: border-box;
+  }
+
+  body{
+    -webkit-font-smoothing: antialiased;
+    background-color: ${theme.colors.black};    
+  }
+
+  body, input, button, select {
+    font: 1rem ''Lexand, sans-serif; // = 16px   (12px/16 = 0.75rem)
+  }
+
+  h1, h2, p, span, strong, button, label, input {
+    line-height: 100%;
+  }
+
+  #root {
+    max-width: 1280px;
+    margin: 0 auto;
+  } 
+
+  button {
+    cursor: pointer;
+  }
+`;
